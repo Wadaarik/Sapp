eef36b0890b45312855b98574af6baeaba7471bc
Aymeric D <loituma@MBP-Loituma.local>
2021-11-23 15:16:49 +0100

    page login

---
+++ login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
+class Login extends React.Component {
+    state = {
+        email: '',
+        password: '',
+        toRedirect: false,
+    }
+
+    submit() {
+        // if(this.state.email === "gerard@mail.fr" && this.state.password === "gerard"){
+        if(this.state.email === "G" && this.state.password === "g"){
+            console.warn(this.state);
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    placeholder="Enter email"
+                    onChangeText={(text) => { this.setState({ email: text }) }}
+                    style={styles.input}
+                    keyboardType="email-address"/>
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(text) => { this.setState({ password: text }) }}
+                    style={styles.input}/>
+
+                <Button title="submit" onPress={() => {this.submit()}}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 2,
+        borderColor:'black',
+        margin:20,
+        padding: 5,
+    },
+});
+
+export default Login;
